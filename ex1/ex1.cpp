73cd4aa0488ac48ce7116bc9305311271ca8c8e2
eduardo <eduardoforsternbc@gmail.com>
2025-04-07 11:43:01 -0300

    aula6

---
+++ ex1/ex1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+
+int osrever(int num) {
+    int reverso = 0;
+    while (num != 0) {
+        int digit = num % 10; 
+        reverso = reverso * 10 + digit; 
+        num /= 10; 
+    }
+    return reverso;
+}
+
+int main() {
+    int number;
+    std::cout << "Digite um numero inteiro: ";
+    std::cin >> number;
+
+    int revertido = osrever (number);
+    std::cout << "O reverso do numero vai ser: " << revertido << std::endl;
+
+    return 0;
+}
